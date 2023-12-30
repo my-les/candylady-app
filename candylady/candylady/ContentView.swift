74fb1f2ddc1002602391700d6fa4eb47ea07ab02
Myles <mcutchember@gmail.com>
2023-12-30 15:34:29 -0800

    first commit

---
+++ candylady/candylady/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  candylady
+//
+//  Created by myle$ on 12/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ candylady/candylady/candyladyApp.swift
@@ -0,0 +1,17 @@
+//
+//  candyladyApp.swift
+//  candylady
+//
+//  Created by myle$ on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct candyladyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
